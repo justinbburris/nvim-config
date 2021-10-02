e483eb81293e41c8d801b1a0d9dd5eed00dc7cdf
Justin Burris <713795+justinbburris@users.noreply.github.com>
2021-10-02 13:19:50 -0400

    more robust setup script

--- setup.sh
+++ setup.sh
@@ -1,3 +1,8 @@
 
+mkdir -p ~/.config/
 rm ~/.config/nvim
 ln -s ~/dev/personal/nvim-config ~/.config/nvim
+
+# setup vimplug
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
